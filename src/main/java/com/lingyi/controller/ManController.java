b9c3b9e3991e598463795b0c477dfa6d3b0eb80d
huang <1635618490@qq.com>
2021-04-26 21:52:58 +0800

    添加man页面

--- src/main/java/com/lingyi/controller/ManController.java
+++ src/main/java/com/lingyi/controller/ManController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -23,18 +25,17 @@ public class ManController {
     @RequestMapping("/get")
     @ResponseBody
     public List<Man> getAll(){
-        log.info("====this is info 级别日志====");
-        log.warn("====this is warn 级别日志====");
-        log.error("====this is error 级别日志====");
-        log.debug("====this is debug 级别日志====");
-        List<Man> men = manService.selectAll();
-        System.out.println("man:"+men);
-        return men;
+        List<Man> manList = manService.selectAll();
+        log.info("manList:{}",manList);
+        return manList;
     }
 
     @RequestMapping("/index")
-    public String getTeacherIndex(){
-        System.out.println("---man------");
+    public String getTeacherIndex(HttpServletRequest request, HttpServletResponse response
+                                  ){
+        List<Man> manList = manService.selectAll();
+        log.info("manList:{}",manList);
+        request.setAttribute("manList",manList);
         return "man/ManList";
     }
 }
--- src/main/java/com/lingyi/server/BryantStartServer.java
+++ src/main/java/com/lingyi/server/BryantStartServer.java
@@ -1,6 +1,8 @@
 package com.lingyi.server;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,8 +12,11 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(basePackages = "com.lingyi.dao")
 public class BryantStartServer {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(BryantStartServer.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BryantStartServer.class, args);
+        LOGGER.info("===server start now!!!===");
     }
 
 }
